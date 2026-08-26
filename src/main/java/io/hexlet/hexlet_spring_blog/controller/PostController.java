7fe17c32e3814be5cc61ad11a2ac6bdb4993f106
SITrifonov <sitrifonov@beeline.ru>
2026-08-26 19:25:37 +0300

    add ResponseEntity

--- src/main/java/io/hexlet/hexlet_spring_blog/controller/PostController.java
+++ src/main/java/io/hexlet/hexlet_spring_blog/controller/PostController.java
@@ -3,9 +3,11 @@ package io.hexlet.hexlet_spring_blog.controller;
 import io.hexlet.hexlet_spring_blog.model.Post;
 import jakarta.websocket.server.PathParam;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -15,28 +17,34 @@ public class PostController {
     private List<Post> posts = new ArrayList<>();
 
     @GetMapping("/posts")
-    public List<Post> index(@RequestParam(defaultValue = "5") Integer limit) {
+    public ResponseEntity<List<Post>> index(@RequestParam(defaultValue = "5") Integer limit) {
 
-        return posts.stream().limit(limit).toList();
+        var result = posts.stream().limit(limit).toList();
+
+        return ResponseEntity.ok(result);
 
     }
 
     @GetMapping("/posts/{id}")
-    public Optional<Post> showPost(@PathVariable Integer id) {
-        return posts.stream()
+    public ResponseEntity<Post> showPost(@PathVariable Integer id) {
+        var post = posts.stream()
                 .filter(x -> x.getId().equals(id))
                 .findFirst();
+
+        return ResponseEntity.of(post);
 
     }
 
     @PostMapping("/posts")
-    public Post createPost(@RequestBody Post post) {
+    public ResponseEntity<Post> createPost(@RequestBody Post post) {
         posts.add(post);
-        return post;
+        return ResponseEntity
+                .created(URI.create("/posts/" + post.getId()))
+                .body(post);
     }
 
     @PutMapping("/posts/{id}")
-    public Post updatePost(@RequestBody Post data, @PathVariable Integer id) {
+    public ResponseEntity<Post> updatePost(@RequestBody Post data, @PathVariable Integer id) {
         var post = posts.stream()
                 .filter(x -> x.getId().equals(id))
                 .findFirst()
@@ -46,14 +54,18 @@ public class PostController {
         post.setContent(data.getContent());
         post.setTitle(data.getTitle());
 
-        return post;
+        return ResponseEntity.ok(post);
 
     }
 
     @DeleteMapping("/posts/{id}")
-    public void deletePost(@PathVariable Integer id) {
+    public ResponseEntity<Void> deletePost(@PathVariable Integer id) {
 
         posts.removeIf(x -> x.getId().equals(id));
+
+        return ResponseEntity
+                .noContent()
+                .build();
 
     }
 }
