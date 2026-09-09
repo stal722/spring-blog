45dbeb39d50b221072fabaebfc76c87db68a634c
SITrifonov <sitrifonov@beeline.ru>
2026-09-09 18:42:03 +0300

    change UserController

--- src/main/java/io/hexlet/hexlet_spring_blog/controller/UserController.java
+++ src/main/java/io/hexlet/hexlet_spring_blog/controller/UserController.java
@@ -3,6 +3,7 @@ package io.hexlet.hexlet_spring_blog.controller;
 import io.hexlet.hexlet_spring_blog.User;
 import io.hexlet.hexlet_spring_blog.model.UserEntity;
 import io.hexlet.hexlet_spring_blog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,11 +14,10 @@ import java.util.List;
 @RequestMapping("/api")
 public class UserController {
 
-    private final UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+
 
     @GetMapping("/users")
     @ResponseStatus(HttpStatus.OK)
